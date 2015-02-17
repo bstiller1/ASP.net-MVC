aa53cc939bfd613f30ed630bfcfd46c7e2c48a7d
Blake Stiller <stiller.blake@gmail.com>
2015-02-17 17:20:38 -0500

    ASP.net MVC 5
    Started Example added layout with CSS.

---
+++ MvcMusicStore/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /Home/
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+        // GET: /Home/About
+        public ActionResult About()
+        {
+            
+            ViewBag.Message = "Here is my About Page";
+            return View();
+        }
+        // GET:/Home/Contact
+        public String Contact()
+        {
+            return "Here is my Contact Page";
+        }
+    }
+}
